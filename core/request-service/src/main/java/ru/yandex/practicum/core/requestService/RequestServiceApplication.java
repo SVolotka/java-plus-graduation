05a41f50ed5263174e3e24b9b967592942ae0fbf
Sergei Volotka <s.volotka73@gmail.com>
2026-07-12 00:29:31 +0600

    fix: add scanBasePackages for Feign fallbacks

--- core/request-service/src/main/java/ru/yandex/practicum/core/requestService/RequestServiceApplication.java
+++ core/request-service/src/main/java/ru/yandex/practicum/core/requestService/RequestServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "ru.yandex.practicum")
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "ru.yandex.practicum.common.feignClient")
 public class RequestServiceApplication {
