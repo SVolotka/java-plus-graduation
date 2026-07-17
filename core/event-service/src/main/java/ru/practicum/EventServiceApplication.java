7820d25cd059e69b6c2575ae1c6ab014dc99a391
Sergei Volotka <s.volotka73@gmail.com>
2026-07-18 02:47:01 +0600

    fix: centralize service configs in Config Server, fix Gateway bootstrap

--- core/event-service/src/main/java/ru/practicum/EventServiceApplication.java
+++ core/event-service/src/main/java/ru/practicum/EventServiceApplication.java
@@ -4,12 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages = {"ru.practicum", "ru.yandex.practicum"})
+@SpringBootApplication(scanBasePackages = {"ru.practicum", "ru.yandex.practicum.common", "ru.practicum.statsclient"})
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "ru.yandex.practicum.common.feignClient")
-@ComponentScan(basePackages = {"ru.practicum", "ru.practicum.statsclient"})
 public class EventServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(EventServiceApplication.class, args);
